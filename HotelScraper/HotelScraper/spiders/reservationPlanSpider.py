ce7089a3703c2409f0773f5f93af42bf8876e194
Aman Deep Singh <deepamansinghads@gmail.com>
2023-07-19 21:08:02 +0530

    Updated the format of Cancel_Policy

--- HotelScraper/HotelScraper/spiders/reservationPlanSpider.py
+++ HotelScraper/HotelScraper/spiders/reservationPlanSpider.py
@@ -15,8 +15,14 @@ class ReservationplanspiderSpider(scrapy.Spider):
             yield response.follow(planLink, callback=self.parsePlanPage)
     
     def parsePlanPage(self, response):
+        cancel_policy_items = response.css("ul.p-box-confirm-cancel li")
+        cancel_policy = []
+        for item in cancel_policy_items:
+            dt = item.css("dl dt::text").get()
+            dd = item.css("dl dd::text").get()
+            cancel_policy.append(dt + " : " + dd)
         yield {
-            'name': response.css("div.p-listLayout-wrapper div.p-listLayout-detail dt::text").get(),
-            'details': response.css("p.detail-text > span.close + span.open::text").get(),
-            'cancel_policy': response.css("ul.p-box-confirm-cancel li dl dt::text, ul.p-box-confirm-cancel li dl dd::text").getall()
-        }
+            # 'name': response.css("div.p-listLayout-wrapper div.p-listLayout-detail dt::text").get(),
+            # 'details': response.css("p.detail-text > span.close + span.open::text").get(),
+            'cancel_policy': cancel_policy
+        } 
